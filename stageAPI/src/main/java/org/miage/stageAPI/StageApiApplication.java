c39a664dd8aac88a99a59ece36b86735a085cff6
Schneider Guillaume <48088710+guig54@users.noreply.github.com>
2023-01-23 10:48:56 +0100

    Add files via upload
    init projet

---
+++ stageAPI/src/main/java/org/miage/stageAPI/StageApiApplication.java
@@ -0,0 +1,13 @@
+package org.miage.stageAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StageApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StageApiApplication.class, args);
+	}
+
+}
